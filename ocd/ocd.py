c5cba57bf236ee8dcef77fa8c623a7986a5fc97a
gditrani269 <gditrani269@gmail.com>
2020-09-23 14:27:21 -0300

    gdd2-1

--- ocd/ocd.py
+++ ocd/ocd.py
@@ -27,13 +27,13 @@ DEPLOYMENT_NAME = "nginx-ejemplo"
 # configuro el cliente de kubernetes
 configuration = kubernetes.client.Configuration()
 #configuration.api_key['authorization'] = os.environ['TEMP_KEY']
-configuration.api_key_prefix['authorization'] = 'Bearer'
-configuration.host = "https://console-openshift-console.apps.us-west-1.starter.openshift-online.com"
-configuration.verify_ssl = False
+#configuration.api_key_prefix['authorization'] = 'Bearer'
+#configuration.host = "https://console-openshift-console.apps.us-west-1.starter.openshift-online.com"
+#configuration.verify_ssl = False
 
 # creo instancias para la api
-api_client = kubernetes.client.ApiClient(configuration)
-AppsV1instance = kubernetes.client.AppsV1Api(api_client)
+#api_client = kubernetes.client.ApiClient(configuration)
+#AppsV1instance = kubernetes.client.AppsV1Api(api_client)
 # GDD-END2
 
 @application.route('/health')
